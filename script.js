fa3e91f9e9e986a3ce90146c68acb7d6f485fe9b
crealyx <sanberk5416@hotmail.com>
2021-04-29 16:28:59 +0300

    Add book inspection feature and additional styles

--- script.js
+++ script.js
@@ -1,68 +1,103 @@
+// Get html elements
+let shelf = document.querySelector('.wrapper');
+let bookPopup = document.querySelector('.book-popup');
 let topShelf = document.querySelector('.top-shelf');
 let addButton = document.querySelector('#add');
 let popUp = document.querySelector('.popup');
 let popUpClose = document.querySelector('.popup-close');
+let authorInput = document.querySelector('#author');
+let titleInput = document.querySelector('#title');
+let pagesInput = document.querySelector('#pages');
+let form = document.querySelector('.form');
+let radioYes = document.querySelector('#yes');
+let radioNo = document.querySelector('#no');
+
 
 
 let library = [];
+let author = '';
+let title = '';
+let pages = '';
+let read = '';
+let book;
 
-// let author = 'Allah cCc';
-// let title = 'Kuran-ı Kerim';
-// let pages = '600';
-// let read = 'Hatim edildi';
+
+
+shelf.addEventListener('click', e => {
+    if(e.target.classList.contains('card') || e.target.classList.contains('cardText')){
+        console.log(e.target);
+        bookPopup.classList.add('book-active');
+    }
+    else if(e.target.classList.contains('book-popup')){
+        bookPopup.classList.remove('book-active')
+    }
+})
+
+// Add custom book to shelf
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    if(authorInput.value !== '' || titleInput.value !== '' || pagesInput.value !== '' || radioYes.checked !== false || radioNo.checked !== false){
+        author = authorInput.value;
+        title = titleInput.value;
+        pages = pagesInput.value;
+        popUp.classList.remove('popup-active');
+        book = new Book(author,title,pages,read);
+        addToLibrary(book);
+    }
+    console.log(library);
+});
+
 
 // Activate popup
 addButton.addEventListener('click', () => {
     popUp.classList.add('popup-active');
-})
+});
 
-popUp.addEventListener('click', ev => {
-    if(ev.target.tagName === 'DIV' || ev.target.tagName === 'SPAN'){
-        console.log(ev.target.tagName);
+// Remove popup
+popUp.addEventListener('click', e => {
+    if(e.target.tagName === 'DIV' || e.target.tagName === 'SPAN'){
         popUp.classList.remove('popup-active')
     }
 })
 
-let book = new Book(`${book.author},${book.title},${book.pages}, ${book.read}`);
-addToLibrary(book);
-addToShelf(library);
-
-console.log(library);
-
-
-
-
-
-
-
-
-
+// Add book object to library array
 function addToLibrary(object) {
     library.push(object);
+    createBook(book);
+}
+// Book object constructor
+function Book(a,b,c,d) {
+    this.author = a;
+    this.title = b;
+    this.pages = c;
+    this.read = d;
 }
 
-// Constructor
-function Book(author,title,pages,read) {
-    this.author = author;
-    this.title = title;
-    this.pages = pages;
-    this.read = read;
+// Add book to shelf
+// function addToShelf(arr){
+//     arr.forEach((book) => {
+//         createBook(book);
+//     })
+// }
+
+
+
+
+// Create book visually
+function createBook(book){
+    const card = document.createElement('div');
+    const cardText = document.createElement('p');
+    const cardRemove = document.createElement('span');
+    cardRemove.textContent = 'X';
+    cardRemove.classList.add('book-remove');
+    card.append(cardRemove);
+    cardText.innerHTML = `${book.author}<br>
+    ${book.title}<br>
+    ${book.pages}<br>
+    ${book.read}`;
+    card.classList.add('card');
+    cardText.classList.add('cardText');
+    topShelf.append(card);
+    card.append(cardText);
+    book = new Book(`${book.author},${book.title},${book.pages}, ${book.read}`);
 }
-
-function addToShelf(arr){
-    arr.forEach((book) => {
-        const card = document.createElement('div');
-        const cardText = document.createElement('p');
-        cardText.innerHTML = `${book.author}<br>
-        ${book.title}<br>
-        ${book.pages}<br>
-        ${book.read}`;
-        card.classList.add('card');
-        cardText.classList.add('cardText');
-        topShelf.append(card);
-        card.append(cardText);
-        book = new Book(`${book.author},${book.title},${book.pages}, ${book.read}`);
-    })
-}
-
-
